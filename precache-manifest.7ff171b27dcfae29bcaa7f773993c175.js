c3449c2050c955d8b6dff82932d519d3ffa073ba
igor29 <igmatviytsev@gmail.com>
2020-03-25 08:37:01 +0200

    Updates

---
+++ precache-manifest.7ff171b27dcfae29bcaa7f773993c175.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716a031b6f313a0eebb9e152662d40b7",
+    "url": "/cookbook/index.html"
+  },
+  {
+    "revision": "746117938cc28648bf46",
+    "url": "/cookbook/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "883ebf2760fcc387ce82",
+    "url": "/cookbook/static/css/main.2acc1eff.chunk.css"
+  },
+  {
+    "revision": "746117938cc28648bf46",
+    "url": "/cookbook/static/js/2.b91d0290.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/cookbook/static/js/2.b91d0290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883ebf2760fcc387ce82",
+    "url": "/cookbook/static/js/main.4dea5146.chunk.js"
+  },
+  {
+    "revision": "00bbaa9f9de6059476b9",
+    "url": "/cookbook/static/js/runtime-main.51ed3048.js"
+  },
+  {
+    "revision": "07034b8aaac9c3bb913a996e67e20b21",
+    "url": "/cookbook/static/media/logo.07034b8a.png"
+  }
+]);
